2c64f291421b2e5db8b1931a899c1fb4253de11a
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-04-18 15:26:46 +0530

    Add files via upload

---
+++ Program2ndday.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter the value of A: ");
+//        int A = int.Parse(Console.ReadLine());
+
+//        Console.WriteLine("Enter the value of B: ");
+//        int B = int.Parse(Console.ReadLine());
+
+//        int sum = A + B;
+
+//        Console.WriteLine("The sum of A and B is: " + sum);
+
+//    }
+//}
