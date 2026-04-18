b33b85ad62543c7282c08ba8a8f6c26dc5de555b
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-04-18 15:24:19 +0530

    Add files via upload

---
+++ Stack_Collections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wipro_training
+{
+    class Stack_Collections
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("Apple");
+            stack.Push("banana");
+            stack.Push("Cherry");
+
+            Console.WriteLine("Stack contents:");
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Is there any item you want to add??");
+            string newItem = Console.ReadLine();
+            stack.Push(newItem);
+
+            List<string> list = new List<string>();
+            foreach (string item in stack)
+            {
+                list.Add(item);
+            }
+            list.Remove("Cherry");
+            Console.WriteLine("Updated list contents:");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            stack.Clear();
+            Console.WriteLine("New and Updted stack contents:");
+            foreach(string item in list)
+            {
+                stack.Push(item);
+            }
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }   
+    }
+}
