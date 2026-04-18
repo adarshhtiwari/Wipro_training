e438997038618017cd1d403c4fca221bc31ed477
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-04-18 14:23:14 +0530

    Add files via upload

---
+++ Collections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wipro_training
+{
+    class Collections
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> names = new List<string>();
+            names.Add("Alice");
+            names.Add("Bob");
+            names.Add("Charlie");
+            Console.WriteLine("Names in the list:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.Remove("Bob");
+            Console.WriteLine("Names in the New list:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            names.Add("Adarsh");
+
+            Console.WriteLine("Names in the New list:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
