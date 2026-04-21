0028db0aeaafa6fd2031e27f23f339841bc9a1d9
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-04-21 23:00:37 +0530

    Add files via upload

---
+++ Day5/PatternMatching.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace day5
+{
+    public class UseOfTuples
+    
+    {
+        static(double area, double perimeter) CalculateAreaAndPerimeter(double length, double breadth)
+        {
+            double area = length * breadth;
+            double perimeter = 2 * (length + breadth);
+            return (area, perimeter);
+        }   
+        
+        public static void Main()
+        {
+            Console.WriteLine("Tuple is " + CalculateAreaAndPerimeter(5, 5));
+            var result = CalculateAreaAndPerimeter(5, 5);
+            Console.WriteLine("Area: " + result.area + ", Perimerter: " + result.perimeter);
+
+        }
+    }
+}
---
+++ Day5/Switchcase_DEMO.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter a value:");
+string input = Console.ReadLine();
+object value;
+
+if (int.TryParse(input, out int i))
+    value = i;
+else if (double.TryParse(input, out double d))
+    value = d;
+else
+    value = input;
+
+
+switch (value)
+{
+    case int intValue:
+        Console.WriteLine($"The input is an integer: {intValue}");
+        break;
+    case double doubleValue:
+        Console.WriteLine($"The input is a double: {doubleValue}");
+        break;
+    case string strValue:
+        Console.WriteLine($"The input is a string: {strValue}");
+        break;
+    default:
+        Console.WriteLine("The input is of an unknown type.");
+        break;
+}
