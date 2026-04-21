596d89f8e98e734c48ce5dd15a3da7093c11257e
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-04-21 23:16:46 +0530

    Add files via upload

---
+++ Day6/MulticasteDelegates_DEMO/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+class Program
+{
+    // Delegate
+    public delegate void MyDelegate();
+
+    // Methods
+    static void Method1()
+    {
+        Console.WriteLine("Method 1 executed");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("Method 2 executed");
+    }
+
+    static void Main()
+    {
+        MyDelegate del = Method1;   // assign first method
+        del += Method2;  // add second method
+
+        del();  // calls both methods
+    }
+}
