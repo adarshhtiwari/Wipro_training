d444ee4ee9a42b68abff57a21ad1669371025f0f
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-03 23:40:52 +0530

    Add files via upload

---
+++ day11/Test1.cs
@@ -0,0 +1,27 @@
+﻿namespace NewDEMO.tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void AddTwoNumbers()
+    {
+        int result = 2 + 3;
+        Assert.AreEqual(5, result);
+    }
+
+    [TestMethod]
+    public void CheckName()
+    {
+        string name = "Alice";
+        Assert.AreEqual("Alice", name);
+    }
+
+    [TestMethod]
+    public void CheckIsTrue()
+    {
+        bool isAdult = true;
+        Assert.IsTrue(isAdult);
+        Console.WriteLine("Alice is an adult.");
+    }
+}
