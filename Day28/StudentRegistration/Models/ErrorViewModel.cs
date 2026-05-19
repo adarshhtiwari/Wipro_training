3a35a2e5db754e3081f7e08c45838fe4992587d0
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 15:50:00 +0530

    Delete Day28/StudentRegistration directory

--- Day28/StudentRegistration/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentRegistration.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
