1085de915b6d729221503c5d0b17466a4622e195
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 16:19:32 +0530

    Add files via upload

---
+++ Day18/CourseRegistrationApp/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseRegistrationApp.Models
+ 
+{//This is my model class with fields where validation will be implemted with
+ //data annotations 
+    public class Student
+    {
+        [Required(ErrorMessage ="Name is required..!!")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Email Is required..!!")]
+        public string Email { get; set; }
+
+        [Range (18,60, ErrorMessage ="Age must be between 18 to 60")]
+        public int Age { get; set; }
+
+    }
+}
