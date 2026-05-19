bd806a2332050d805102ed569c9cf8482a89df03
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 15:46:30 +0530

    Add files via upload

---
+++ Day26/EFDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentRepositoryDemo.Models;
+
+namespace StudentRepositoryDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext
+        (
+            DbContextOptions<ApplicationDbContext> options
+        ) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
