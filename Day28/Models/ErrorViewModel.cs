e53c9508643ed9ba584c771fb961f00c1ba1661f
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 15:48:52 +0530

    Delete Day28/Models directory

--- Day28/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentAjaxApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
