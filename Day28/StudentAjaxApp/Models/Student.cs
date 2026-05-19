6064b096e5af8a250e427d1694580e43db053b4a
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 16:13:10 +0530

    Add files via upload

---
+++ Day28/StudentAjaxApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentAjaxApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+
+        public int Age { get; set; }
+    }
+}
