d9eeda24f4dfeae167666285793cbcc0261975b5
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 16:27:29 +0530

    Add files via upload

---
+++ Day19/ValidationDemoApp/ValidationDemoApp/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ValidationDemoApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult REGISTER()
+        {
+            return View();
+        }
+    }
+    public IActionResult: Controller
+
+
+   public IActionResult Register(Student model)
+   {
+        if(!ModelState.IsValid) {
+            return View(model);
+        }
+
+        ViewBag.Message = "Registration successful!";
+        return View("Success");
+    }
+
+}
+
---
+++ Day19/ValidationDemoApp/ValidationDemoApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+namespace ValidationDemoApp.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Range (18, 60)]
+        public int Age { get; set; }
+    }
+}
