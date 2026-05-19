eab003cc05fdf7bca0f22e799ffabfbf0e0cadde
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 15:42:33 +0530

    Delete Day26/EFDemo/StudentRepositoryDemo directory

--- Day26/EFDemo/StudentRepositoryDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentRepositoryDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
