0a282e1b21ad64766b023d66e605ac01ff3a66ba
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-05-19 15:48:17 +0530

    Add files via upload

---
+++ Day28/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAjaxApp.Models;
+
+namespace StudentAjaxApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Dummy student data
+        List<Student> students = new List<Student>()
+        {
+            new Student { StudentId = 101, Name = "Rahul", Course = "CSE", Age = 21 },
+            new Student { StudentId = 102, Name = "Aman", Course = "ECE", Age = 22 },
+            new Student { StudentId = 103, Name = "Priya", Course = "IT", Age = 20 }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // AJAX METHOD
+        [HttpGet]
+        public JsonResult SearchStudent(int id)
+        {
+            var student = students.FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Student Not Found"
+                });
+            }
+
+            return Json(new
+            {
+                success = true,
+                data = student
+            });
+        }
+    }
+}
