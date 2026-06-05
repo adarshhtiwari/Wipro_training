1fda51c750cdb57916f06e38af2385499d82ab05
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-06-05 12:27:52 +0530

    Add files via upload

---
+++ Day34/MicroservicesDemo/OrderService/OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(new[]
+            {
+                "Order101",
+                "Order102"
+            });
+        }
+    }
+}
