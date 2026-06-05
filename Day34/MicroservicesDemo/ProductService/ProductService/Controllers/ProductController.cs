8383c90a3f62f62006c7bfe99fdc74534907d966
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-06-05 12:29:01 +0530

    Add files via upload

---
+++ Day34/MicroservicesDemo/ProductService/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[]
+            {
+                "Laptop",
+                "Mouse",
+                "Keyboard"
+            });
+        }
+    }
+}
