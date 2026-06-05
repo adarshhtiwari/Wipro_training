f35ce7ea807ad2487b73799a53b15e0fb18d938d
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2026-06-05 12:31:04 +0530

    Add files via upload

---
+++ Day34/MicroservicesDemo/UserService/UserService/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(new[]
+            {
+                "Adarsh",
+                "Rahul"
+            });
+        }
+    }
+}
